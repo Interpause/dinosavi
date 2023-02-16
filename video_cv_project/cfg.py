e8acb5178384e0840364c15af2ce8fffab6f8b09
John-Henry Lim <ljunhanh@dso.org.sg>
2023-02-16 09:56:06 +0000

    rewrite model init & node embedding

---
+++ video_cv_project/cfg.py
@@ -0,0 +1,13 @@
+"""TODO: Add module docstring."""
+
+import torch
+
+__all__ = ["RGB", "RGB_MEAN", "RGB_STD", "BEST_DEVICE"]
+
+RGB = 3
+# NOTE: Both PyTorch & PIL use RGB order so assume everything is RGB.
+
+RGB_MEAN = (0.4914, 0.4822, 0.4465)
+RGB_STD = (0.2023, 0.1994, 0.2010)
+
+BEST_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- video_cv_project/utils/model_hack.py
+++ video_cv_project/utils/model_hack.py
@@ -1,11 +1,13 @@
 """TODO: Add module docstring."""
 
-from typing import Callable, List, TypeVar
+from typing import Callable, List, Tuple, TypeVar
 
 import torch
 import torch.nn as nn
 
-__all__ = ["find_layers", "delete_layers", "override_forward"]
+from video_cv_project.cfg import BEST_DEVICE
+
+__all__ = ["find_layers", "delete_layers", "override_forward", "infer_outdim"]
 
 Self = TypeVar("Self")
 
@@ -46,3 +48,23 @@ def override_forward(
     """
     bound_method = func.__get__(model, model.__class__)
     setattr(model, "forward", bound_method)
+
+
+def infer_outdim(model: nn.Module, indim: Tuple[int], device=BEST_DEVICE):
+    """Infer output dimension of model.
+
+    Args:
+        model (torch.nn.Module): Model to check.
+        indim (Tuple[int]): Input dimension.
+        device (torch.device, optional): Device to use. Defaults to best available.
+
+    Returns:
+        torch.Size: Output dimension.
+    """
+    x = torch.zeros(indim).to(device)
+    model.to(device).eval()
+
+    with torch.inference_mode():
+        y: torch.Tensor = model(x)
+
+    return y.shape

adeaee3b73977dc3d95907898d19b0d1a153604d
John-Henry Lim <ljunhanh@dso.org.sg>
2023-02-23 10:23:20 +0000

    write & test davis dataset loader

--- video_cv_project/data/__init__.py
+++ video_cv_project/data/__init__.py
@@ -1,3 +1,4 @@
 """Data related code such as dataloaders and data pipelines."""
 
 from .kinetics import *
+from .vos import *
--- video_cv_project/data/vos.py
+++ video_cv_project/data/vos.py
@@ -2,6 +2,7 @@
 
 import logging
 from math import ceil
+from pathlib import Path
 from typing import List, Sequence, Tuple
 
 import torch
@@ -11,6 +12,8 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 from video_cv_project.cfg import RGB
+
+__all__ = ["VOSDataset", "DAVISDataset"]
 
 logger = logging.getLogger(__name__)
 
@@ -41,7 +44,7 @@ def find_label_classes(lbl: torch.Tensor) -> torch.Tensor:
     Returns:
         torch.Tensor: NC classes, where N is number of classes & C is the color.
     """
-    return torch.unique(lbl.reshape(-1, RGB), dim=0)
+    return torch.unique(lbl.flatten(1), dim=1).T
 
 
 class VOSDataset(Dataset):
@@ -64,7 +67,7 @@ class VOSDataset(Dataset):
             if im_size == -1 or isinstance(im_size, tuple)
             else (im_size, im_size)
         )
-        self.map_scale = map_scale
+        self.map_scale = map_scale  # Downscale labels to encoder's latent map size.
         self.context_len = context_len
         self.is_frames = is_frames
         # TODO: Support loading videos directly?
@@ -93,9 +96,6 @@ class VOSDataset(Dataset):
         im_dir, im_paths = self.im_dirs[index]
         lbl_dir, lbl_paths = self.lbl_dirs[index]
 
-        im_paths = self.repeat_context(im_paths)
-        lbl_paths = self.repeat_context(lbl_paths)
-
         ims = load_images(im_paths, to_tensor=False)
         lbls = load_images(lbl_paths, to_tensor=True)
 
@@ -103,17 +103,20 @@ class VOSDataset(Dataset):
             im, lbl = ims[i], lbls[i]
 
             if self.im_size != -1:
-                im = F.to_tensor(F.resize(im, self.im_size))
+                im = F.resize(im, self.im_size)
                 lbl = F.resize(
                     lbl, self.im_size, interpolation=T.InterpolationMode.NEAREST
                 )  # Must use nearest to preserve label colors.
 
             # NOTE: Original had option to convert to LAB colorspace here.
 
-            ims[i], lbls[i] = im, lbl
+            ims[i], lbls[i] = F.to_tensor(im), lbl
 
         meta = dict(
-            im_dir=im_dir, lbl_dir=lbl_dir, im_paths=im_paths, lbl_paths=lbl_paths
+            im_dir=im_dir,
+            lbl_dir=lbl_dir,
+            im_paths=self.repeat_context(im_paths),
+            lbl_paths=self.repeat_context(lbl_paths),
         )
 
         lbls = torch.stack(lbls)  # TCHW
@@ -135,8 +138,7 @@ class VOSDataset(Dataset):
             # CHW -> NCHW -> HWNC
             lbl = (lbl * 255).to(torch.uint8).expand(N, -1, -1, -1).permute(2, 3, 0, 1)
             # HWNC == NC -> HWN -> NHW, where N is each class.
-            tgt = torch.all(lbl == lbl_cls, dim=2).permute(2, 0, 1)
-
+            tgt = torch.all(lbl == lbl_cls, dim=3).permute(2, 0, 1)
             # Resize to latent map size. Using smooth scaling is fine here.
             tgt = F.resize(tgt.to(torch.float32), lbl_sz)
 
@@ -144,8 +146,44 @@ class VOSDataset(Dataset):
 
             tgts.append(tgt)
 
+        ims = self.repeat_context(ims)
+        tgts = self.repeat_context(tgts)
+
         return torch.stack(ims), torch.stack(tgts), lbl_cls, meta
 
     def __len__(self):
         """Dataset length."""
         return len(self.im_dirs)
+
+
+class DAVISDataset(VOSDataset):
+    """DAVIS Dataset."""
+
+    def __init__(
+        self,
+        davis_dir: str,
+        year: str = "2017",
+        split: str = "val",
+        quality: str = "480p",
+        im_size: int | Tuple[int, int] = -1,
+        map_scale: int = 8,
+        context_len: int = 20,
+    ):
+        """Initialize DAVIS dataset."""
+        im_dirs = []
+        lbl_dirs = []
+
+        root = Path(davis_dir).resolve()
+        imageset_txt = root / "ImageSets" / year / f"{split}.txt"
+        with imageset_txt.open() as f:
+            videos = {l for s in f.readlines() if (l := s.strip()) != ""}
+
+        for video in videos:
+            lbl_dir = root / "Annotations" / quality / video
+            im_dir = root / "JPEGImages" / quality / video
+            im_paths = sorted(im_dir.glob("*.jpg"), key=lambda p: int(p.stem))
+            lbl_paths = sorted(lbl_dir.glob("*.png"), key=lambda p: int(p.stem))
+            im_dirs.append((str(im_dir), [str(p) for p in im_paths]))
+            lbl_dirs.append((str(lbl_dir), [str(p) for p in lbl_paths]))
+
+        super().__init__(im_dirs, lbl_dirs, im_size, map_scale, context_len, True)

2227ed8b18e65eab5e718f2e9c7cd465af724ed3
John-Henry Lim <ljunhanh@dso.org.sg>
2023-03-21 08:16:42 +0000

    minor changes

--- tests/pipeline.py
+++ tests/pipeline.py
@@ -6,13 +6,15 @@ Can also output image patches for user to understand if its working.
 import torchvision.transforms.functional as F
 from PIL import Image
 
+from video_cv_project.cfg import RGB
+
 
 def _test(im_path: str, pipeline):
     # NOTE: `video_cv_project.data.transform.create_pipeline` has flag ``do_rgb_norm```
     # which should be disabled for this.
     im = F.to_tensor(Image.open(im_path).convert("RGB"))
     ims = pipeline([im])
-    ims = ims.unflatten(0, (-1, 3))
+    ims = ims.unflatten(0, (-1, RGB))
     # Might want to use `torchvision.utils.make_grid` here.
     for i, patch in enumerate(ims):
         F.to_pil_image(patch).save(f"{i}.png")
